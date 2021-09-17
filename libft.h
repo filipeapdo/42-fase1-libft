2e060d17d38be62020cb6085c90175b84da5cf55
fiaparec <fiaparec@student.42sp.org.br>
2021-09-16 21:28:51 -0300

    Pt2: 10 out 11 (str[mapi, iteri])

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fiaparec <fiaparec@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 11:04:23 by fiaparec          #+#    #+#             */
-/*   Updated: 2021/09/16 07:46:05 by fiaparec         ###   ########.fr       */
+/*   Updated: 2021/09/16 21:20:48 by fiaparec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ char	*ft_itoa(int n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strmapi(char const *s, char (f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 // fd functions
 void	ft_putchar_fd(char c, int fd);
